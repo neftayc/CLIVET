3a6a579d9bb6290b71c7ffe693659a35162b718f
rusbel <rusbel.ccana@gmail.com>
2016-10-05 06:41:01 -0500

    avance de las vistas

--- apps/clivet/views.py
+++ apps/clivet/views.py
@@ -5,13 +5,12 @@ from django.http import HttpResponse
 from django.contrib import messages
 from apps.utils.decorators import permission_resource_required
 
+
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from .models import Vacuna
+
 # Create your views here.
 def clivet(request):
-    """
-    """
-    c = {
-        'cmi' : 'clivet:index',
-        'opts': _('Home'),
-        'title': _('Backend Home Page.'),
-    }
-    return render(request, 'clivet/clivet.html', c)
+    vacuna = Vacuna.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'clivet/clivet.html', {'vacuna': vacuna})
