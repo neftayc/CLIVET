ad2c3ff2023a9b87483f3b68a5bceee45e2469b6
neftayc <rusbel.ccana@gmail.com>
2016-11-30 00:33:19 -0500

    Update tests.py

--- apps/clinica/tests.py
+++ apps/clinica/tests.py
@@ -1,3 +1,17 @@
+# coding: utf-8 _*_
 from django.test import TestCase
+from apps.clinica.models.mascota import Mascota
+from apps.clivet.models.cliente import Cliente
+from apps.params.models import Person
 
 # Create your tests here.
+
+class MascotaTestCase(TestCase):
+    def setUp(self):
+        a1 = Person.objects.create(first_name="J.K. Rowling")
+        a2 = Cliente.objects.create(persona=a1)
+        Mascota.objects.create(nombre="tarzan", color="negro", dueño=a2)
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        tarzan = Mascota.objects.get(nombre="tarzan")
