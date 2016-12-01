87e54a39abaf93ac7efc326a9eeab4b2f42b6c35
neftayc <rusbel.ccana@gmail.com>
2016-12-01 05:51:06 -0500

    Update settings.py

--- backengo/settings.py
+++ backengo/settings.py
@@ -94,11 +94,10 @@ ROOT_URLCONF = 'backengo.urls'
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.run_pep8',
-    #'django_jenkins.tasks.run_pep8',
-    #'django_jenkins.tasks.run_pyflakes',
-    #'django_jenkins.tasks.run_jslint',
-    #'django_jenkins.tasks.run_csslint',
-    #'django_jenkins.tasks.run_sloccount'
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_jslint',
+    'django_jenkins.tasks.run_csslint',
+    'django_jenkins.tasks.run_sloccount'
 )
 
 TEMPLATES = [
